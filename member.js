04df15cdd10fb0b1ca2c6faf33546a0ab62b1c8c
Sergio Ruiz <59260822+sruizs-dev@users.noreply.github.com>
2024-06-18 03:04:25 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,7 +1,8 @@
-function skillsmembers (req, res) {
-  if (req.method === 'GET') {
-    res.end('GET request to the homepage');
-  } else if (req.method === 'POST') {
-    res.end('POST request to the homepage');
-  }
+function skillsMember() {
+  var member = {
+    name: "John",
+    age: 25,
+    skills: ["JavaScript", "React", "Node", "MongoDB"],
+  };
+  return member;
 }
