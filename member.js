bb6ffb7cbab7ad5a347d8fad6da8bf8a33adad65
Sergio Ruiz <59260822+sruizs-dev@users.noreply.github.com>
2024-06-17 22:02:47 -0500

    Delete member.js

--- member.js
+++
@@ -1,7 +0,0 @@
-function skillsmembers (req, res) {
-  if (req.method === 'GET') {
-    res.end('GET request to the homepage');
-  } else if (req.method === 'POST') {
-    res.end('POST request to the homepage');
-  }
-}
